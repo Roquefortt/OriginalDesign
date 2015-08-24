d28152403220b8604aabdb6c28310bae77ddb279
Roquefortt <xpiezx@gmail.com>
2015-08-24 11:42:41 -0700

    index.html

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,10 +1,30 @@
+int y = 150;
+
 void setup()
 {
+	frameRate(30);
 
+	size(200,200); 
+	noStroke();
+	fill(173,140,115);
+	rect(0,130,200,50);
 }
+
 void draw()
 {
-	fill(255,0,0);
-	ellipse(50,50,50,50);
+	y--;
+
+	noStroke();
+	fill(74,140,56);
+	ellipse(100,y,5,15);
+
+	if (y <100) {
+		noLoop();
+
+		fill(64,175,202);
+		ellipse(100,90,15,15);
+
+	}
+
 }
 
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,14 +14,34 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+int y = 150;
+
 public void setup()
 {
+	frameRate(30);
 
+	size(200,200); 
+	noStroke();
+	fill(173,140,115);
+	rect(0,130,200,50);
 }
+
 public void draw()
 {
-	fill(255,0,0);
-	ellipse(50,50,50,50);
+	y--;
+
+	noStroke();
+	fill(74,140,56);
+	ellipse(100,y,5,15);
+
+	if (y <100) {
+		noLoop();
+
+		fill(64,175,202);
+		ellipse(100,85,15,15);
+
+	}
+
 }
 
   static public void main(String[] passedArgs) {
